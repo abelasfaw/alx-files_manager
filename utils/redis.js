67f94828cfc045ee7cb1e403aedbd942ddef3f65
abelasfaw <abeloman89@gmail.com>
2023-05-29 12:34:11 -0700

    fix lint errors

--- utils/redis.js
+++ utils/redis.js
@@ -12,7 +12,7 @@ class RedisClient {
     });
 
     this.client.on('connect', () => {
-      console.log("Redis connection successful")
+      console.log('Redis connection successful');
     });
   }
 
